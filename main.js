5014694c07226f02d57246cf5edf3273c7076b4e
“Kimberly <kimberly.achkio@gmail.com>
2018-12-18 11:58:02 -0700

    Create card template in js

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 function generateIdeaCard() {
-  var element = document.createElement('section');
-  element.className = 'idea-card';
-  element.innerHTML =
-
+  var card = document.createElement('section');
+  var cardContainer = document.querySelector('.card-container');
+  card.className = 'idea-card';
+  card.innerHTML = 
+  `<article id=${ideaObject.id} class="card-container">
+    <h2 contenteditable = true class="card-title">
+    ${ideaObject.title}
+    </h2>  
+    <article contenteditable = true class="card-body">
+    ${ideaObject.body}
+    </article>
+    <hr>
+    <footer class="idea-card-footer">
+      <img class="downvote-button" <i> >
+      <img class="upvote-button" <i> >
+      Quality: ${ideaObject.qualityArray[ideaObject.qualityIndex]}
+      <img class="delete-button" <i> >
+    </footer>
+  </article>
+  `
+  cardContainer.prepend(card);
 }
